2dfd7c42800057f7c2d25566c34e88a05d8da872
Lukas Marx <marxlukas@googlemail.com>
2018-11-29 11:18:16 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Date :date="new Date()"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Date from "./components/Date.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Date
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Date.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p>Computed: {{formatedDate}}</p>
+    <p>Method: {{formatDate()}}</p>
+    <p>{{random}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Date",
+  props: {
+    date: Date
+  },
+  data() {
+    return {
+      random: 1
+    };
+  },
+  created() {
+    setInterval(() => {
+      this.random = Math.random();
+    }, 500);
+  },
+  computed: {
+    formatedDate() {
+      console.log("computed");
+      let options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      };
+      return this.date.toLocaleString("en-us", options);
+    }
+  },
+  methods: {
+    formatDate() {
+      console.log("method");
+      let options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      };
+      return this.date.toLocaleString("en-us", options);
+    }
+  }
+};
+</script>
